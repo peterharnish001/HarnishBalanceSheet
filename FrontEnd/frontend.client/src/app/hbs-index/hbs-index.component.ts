01e5224117a59ffda8e3198a289e431fc7f99221
peterharnish <peterharnisch@hotmail.com>
2026-02-21 13:01:18 -0800

    hbs-index component

--- FrontEnd/frontend.client/src/app/hbs-index/hbs-index.component.ts
+++ FrontEnd/frontend.client/src/app/hbs-index/hbs-index.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
+import { HttpClient } from '@angular/common/http';
+import DataTables from 'datatables.net';
 
 @Component({
   selector: 'app-hbs-index',
   standalone: true,
   templateUrl: './hbs-index.component.html',
   styleUrl: './hbs-index.component.css',
+  imports: [ DataTablesModule ]
 })
-export class HbsIndexComponent {
+export class HbsIndexComponent implements OnInit {
+ dtOptions: DataTables.Settings = {};
 
+constructor(private http: HttpClient) {}
+
+ ngOnInit() {
+  this.dtOptions = {
+      ajax: 'http://localhost:5298/api/index/balancesheets?count=1',
+      pagingType: 'full_numbers',
+      pageLength: 24,
+      processing: true,
+      responsive: true,
+      order: [[1, 'desc']],
+      columnDefs: [
+        { targets: [1], orderable: false }
+      ]
+    };
+
+    this.http.get('http://localhost:5298/api/index/balancesheets?count=1')
+      .subscribe(response => {
+        console.log(response);
+      });
+ }
 }
